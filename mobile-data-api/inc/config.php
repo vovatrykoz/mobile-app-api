b58cdc96833654f0a557823512775f8d339244de
vovatrykoz <88552890+vovatrykoz@users.noreply.github.com>
2022-12-14 16:43:03 +0100

    Delete mobile-data-api directory

--- mobile-data-api/inc/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-//Defining constants that will be used when connecting to the database.
-
-define('DB_SERVER', 'pro.freedb.tech');
-define('DB_USERNAME', 'learningappuser');
-define('DB_PASSWORD', 'xRg8py#Q2AWB8t#');
-define('DB_NAME', 'learningappdatabase');
-?>
